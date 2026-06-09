13d840621aea36e4a6832aff41be0858f03f79f0
leoogomez7 <campeonleonardo@hotmail.com>
2026-06-09 12:32:47 -0300

    posible solucion 404 web

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 import * as serverModule from "../dist/server/server.js";
 const server = serverModule.default ?? serverModule;
 
+const RequestImpl = globalThis.Request ?? (await import("node:undici")).Request;
+
 export default async function handler(req, res) {
   const host = req.headers.host || "localhost";
   const url = new URL(req.url, `https://${host}`);
 
-  const request = new Request(url.toString(), {
+  const request = new RequestImpl(url.toString(), {
     method: req.method,
     headers: req.headers,
     body: req.method === "GET" || req.method === "HEAD" ? undefined : req,
